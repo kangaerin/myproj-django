1cd06f46d7ffa192c7259693d937130f9373d4a4
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-06 17:15:28 +0900

    마이그레이션 문제로 모델 재생성, review CRUD api 구현

--- myproj/settings.py
+++ myproj/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third_app
+    'rest_framework',
     # local app
     'shop',
 ]
