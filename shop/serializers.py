84d6e4611341ffd549fc22467237e5d145183e6e
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-07 10:13:56 +0900

    해설 주석 추가

--- shop/serializers.py
+++ shop/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from shop.models import Review
 
 
+# 장고의 기능만이 아닌 drf 기능을 활용함
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,7 @@ from shop.models import Review
 from shop.serializers import ReviewSerializer
 
 
+# list detail delete update까지 한 번에 구현됨.
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
