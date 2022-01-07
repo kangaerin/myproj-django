4a3e7fc93271ceb38ca26b85591ee99c70e8522c
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-07 13:46:57 +0900

    djanfo-cors-headers를 적용해 localhost3000을 적용

--- myproj/settings.py
+++ myproj/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # third_app
     'rest_framework',
+    'corsheaders',
     # local app
     'shop',
 ]
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,3 +129,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#django-cors-headers
+#https://github.com/adamchainz/django-cors-headers
+
+CORS_ALLOWED_ORIGINS =["http://localhost:3000"]
