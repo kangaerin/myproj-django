275f2b3fd628836c50b245e27350d27f2d122896
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-11 19:54:17 +0900

    누락된 db_index 추가

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,13 @@
 #blog/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from blog.views import PostViewSet
 
 app_name = "blog"
 
-urlpatterns = []
+router = DefaultRouter()
+router.register("posts", PostViewSet)
+
+urlpatterns = [
+    path("api/", include(router.urls)),
+]
