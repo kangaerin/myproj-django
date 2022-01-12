bb9cb44cbc7234af798e1bef4bd0a1fe854b7a9a
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-12 10:30:16 +0900

    news.Article 모델/ 마이그레이션 생성

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from news.models import Article
 
-# Register your models here.
+admin.site.register(Article)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["id", "title"]
+    list_display_links = ["title"]
+    search_fields = ["title"]
