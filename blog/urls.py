2c083a201cf3360a08ab71f5b69e910ab920bfa0
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-13 15:08:01 +0900

    posts.json 으로 응용

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from blog.views import PostViewSet
+from blog import views
 
 app_name = "blog"
 
@@ -9,5 +10,6 @@ router = DefaultRouter()
 router.register("posts", PostViewSet)
 
 urlpatterns = [
+    path("posts.json", views.blog_list),
     path("api/", include(router.urls)),
 ]
