f082251d14f89ca98adb572d8614f020162a2edf
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-13 16:58:41 +0900

    form/serializer 비교 및 유효성 검사

--- news/serializers.py
+++ news/serializers.py
@@ -1,3 +1,5 @@
+import re
+
 from rest_framework import serializers
 from news.models import Article
 
@@ -9,11 +11,13 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = "__all__"
-
-    def validate_title(self, title):
-        if len(title) < 3:
-            raise serializers.ValidationError("3글자 이상!!!")
-        return title
+    #
+    # def validate_title(self, title):
+    #     if len(title) < 3:
+    #         raise serializers.ValidationError("3글자 이상!!!")
+    #     if not re.search(r"[ㄱ-힣]", title):
+    #         raise serializers.ValidationError("한글을 한 글자 이상 포함시켜주세요.")
+    #     return title
 
 
 class ArticleAnonymousSerializer(serializers.ModelSerializer):
