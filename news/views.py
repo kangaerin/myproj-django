39da668dc0a87a3bd583b117236f1721bb90d3d6
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-13 15:21:53 +0900

    serializer를 이용해 #2

--- news/views.py
+++ news/views.py
@@ -13,13 +13,20 @@ class ArticleViewSet(ModelViewSet):
 
 def article_list(request):
     qs = Article.objects.all()
-    data = [
-        {
-            "id": article.id,
-            "title": article.title,
-            "content": article.content,
-        }
-        for article in qs
-    ]
+
+    # 2
+    serializer = ArticleSerializer(qs, many=True)
+    data = serializer.data
+
+    # 1
+    # data = [
+    #     {
+    #         "id": article.id,
+    #         "title": article.title,
+    #         "content": article.content,
+    #         "photo": request.build_absolute_uri(article.photo.url) if article.photo else None,
+    #     }
+    #     for article in qs
+    # ]
     json_string = json.dumps(data)
-    return HttpResponse(json_string)
+    return HttpResponse(json_string, content_type="application/json")
