f0b1cf106de98fb84278e98d084b3cbb9e5fdfc0
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-18 16:20:38 +0900

    회원가입 API 구현

--- news/serializers.py
+++ news/serializers.py
@@ -1,16 +1,25 @@
 import re
 
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from news.models import Article
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "first_name", "last_name"]
 
 
 # 상황에 따라 다르게 선언이 가능함. 선언적 UI
 # 비로그인 사용자용
 
 class ArticleSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+
     class Meta:
         model = Article
-        fields = ["id", "title", "content", "photo"]
+        fields = ["id", "title", "content", "photo", "author"]
     #
     # def validate_title(self, title):
     #     if len(title) < 3:
@@ -18,23 +27,23 @@ class ArticleSerializer(serializers.ModelSerializer):
     #     if not re.search(r"[ㄱ-힣]", title):
     #         raise serializers.ValidationError("한글을 한 글자 이상 포함시켜주세요.")
     #     return title
-
-
-class ArticleAnonymousSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = ["id", "title", "content"]
-
-
-# 골드멤버쉽 사용자용
-class ArticleGoldMemberShipSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = ["id", "title", "content", "photo"]
-
-
-# 관리자용
-class ArticleAdminSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = ["id", "title", "content", "photo", "created_at", "updated_at"]
+#
+#
+# class ArticleAnonymousSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Article
+#         fields = ["id", "title", "content"]
+#
+#
+# # 골드멤버쉽 사용자용
+# class ArticleGoldMemberShipSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Article
+#         fields = ["id", "title", "content", "photo"]
+#
+#
+# # 관리자용
+# class ArticleAdminSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Article
+#         fields = ["id", "title", "content", "photo", "created_at", "updated_at"]
